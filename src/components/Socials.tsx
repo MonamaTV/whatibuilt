cbf240ac25e7285a6dafa1b0fc1a51d84f749b65
Tadima <19066791722k23@bootcamp.wethinkcode.co.za>
2023-05-09 16:08:18 +0200

    pushing error boundaries

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -11,7 +11,7 @@ const Platforms = ({
   channels: Channels;
   closeModal: () => void;
 }) => {
-  const [platforms, setPlatforms] = useState<Channels>(channels);
+  const [platforms, setPlatforms] = useState<Channels>(channels ?? null);
 
   const handleChannelUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
